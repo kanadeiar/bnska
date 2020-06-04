97d26e3f87310329b618ab9b598dc45d9bac2d81
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-06-04 14:11:28 +0300

    add openfilepath dialogs

--- BnskaSolution/Bnska1/MainWindow.xaml.cs
+++ BnskaSolution/Bnska1/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Configuration;
+using Microsoft.Win32;
+using System.IO;
 
 namespace Bnska1
 {
@@ -30,10 +32,87 @@ namespace Bnska1
         {
             
         }
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             
+        }
+        void OpenFilePathDialog(TextBox textbox)
+        {
+            OpenFileDialog dialog = new OpenFileDialog
+            {
+                Filter = "Файлы из БНСки (*.csv)|*.csv",
+                FileName = textbox.Text,
+            };
+            if (dialog.ShowDialog() == true)
+            {
+                textbox.Text = dialog.FileName;
+            }
+        }
+        private void ButtonPathCSV1_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFilePathDialog(textPathCSV1);
+        }
+        private void ButtonPathCSV2_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFilePathDialog(textPathCSV2);
+        }
+        private void ButtonPathCSV3_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFilePathDialog(textPathCSV3);
+        }
+        private void ButtonPathCSV4_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFilePathDialog(textPathCSV4);
+        }
+        private void ButtonPathCSV5_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFilePathDialog(textPathCSV5);
+        }
+        private void ButtonPathCSV6_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFilePathDialog(textPathCSV6);
+        }
+        private void ButtonConvert1_Click(object sender, RoutedEventArgs e)
+        {
+            string pathcsv = textPathCSV1.Text;
+            string pathxslx = textPathXSLX1.Text;
+            if (string.IsNullOrEmpty(pathcsv))
+            {
+                MessageBox.Show("Введите путь к csv-файлу из БНСки!","Внимательней!");
+                return;
+            }
+            if (string.IsNullOrEmpty(pathxslx))
+            {
+                MessageBox.Show("Введите путь к xslx-файлу графика Майорова!", "Внимательней!");
+                return;
+            }
+            if (!File.Exists(pathcsv))
+            {
+                MessageBox.Show($"Не существует csv-файла БНСки {pathcsv}","Ошибка");
+                return;
+            }
+            try
+            {
+                Converter.ConvertCSVToXLSX(pathcsv, pathxslx);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Невозможно произвести экспорт данных в файл Excel\n" + ex.Message, "Ошибка");
+                return;
+            }
+            MessageBox.Show("Экспорт данных в файл Excel завершен успешно.", "Порядок");
+        }
+        private void ButtonPathXSLX1_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog
+            {
+                Filter = "Файлы Mictosoft Excel (*.xslx)|*.xlsx",
+                FileName = textPathXSLX1.Text,
+            };
+            if (dialog.ShowDialog() == true)
+            {
+                textPathXSLX1.Text = dialog.FileName;
+            }
         }
     }
 }
